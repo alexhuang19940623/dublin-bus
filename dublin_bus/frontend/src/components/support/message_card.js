f7c74b6f1d6a02cbd12bf144c13c54bcf3ddabc6
Alex <tianyu.huang@ucdconnect.ie>
2022-08-02 11:20:28 +0100

    change

--- dublin_bus/frontend/src/components/support/message_card.js
+++ dublin_bus/frontend/src/components/support/message_card.js
@@ -53,7 +53,7 @@ export function MessageCard({ item, is_super_user, contents, set_contents }) {
             </div>
             <div className="reply">
                 {item.reply != "" ? (
-                    <span>{item.email} reply at {item.reply_time}:</span>
+                    <span>Admin reply at {item.reply_time}:</span>
                 ) : (
                     <span>no reply yet</span>
                 )}
